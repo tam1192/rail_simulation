8261deaf009ae5286f15f8b44270fdace3fd3396
tam1192 <tamkame123@outlook.com>
2026-08-09 00:42:45 +0900

    feat: wasm環境を作成、連携

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import RailSimulation from "./components/RailSimulation.vue";
+import init, { greet } from "../wasm/pkg";
+
+const wasmReady = ref(false);
+
+onMounted(async () => {
+    try {
+        await init();
+        wasmReady.value = true;
+    } catch (error) {
+        console.error(error);
+        wasmReady.value = false;
+    }
+})
+
+function button() {
+    // WASMの準備ができていない場合は実行をガードする
+    if (!wasmReady.value) {
+        console.warn('WASM is not ready yet.')
+        return
+    }
+
+    greet('hello')
+}
+
 </script>
 
 <template>
     <main>
         <h1>鉄道台車・余弦引き込みシミュレーション</h1>
         <RailSimulation />
+        <button :disabled="!wasmReady" @click="button"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">button</button>
     </main>
 </template>
 
